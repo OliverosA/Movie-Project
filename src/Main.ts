b6ccd80e62bf88cf587ec505fced9c3c84a81717
OliverosA <aldair_oliveros09@hotmail.es>
2022-07-03 22:00:46 -0600

    creando los formularios

--- src/Main.ts
+++ src/Main.ts
@@ -22,7 +22,34 @@ type HorrorMovieInput = {
 }
 
 export default class Main {
-  movies: Movie[] = []; 
+  movies: Movie[] = [];
+  menuOptions: SelectChoice[] = [
+    { option: 1, message: 'Add Action Movie' },
+    { option: 2, message: 'Add Horror Movie' },
+    { option: 3, message: 'Show Action Movies' },
+    { option: 4, message: 'Show Horror Movies' },
+    { option: 5, message: 'Increment Explosions In Movie' },
+    { option: 6, message: 'Increment Jump Scares In Movie' },
+    { option: 7, message: 'Exit' },
+  ];
+
+  actionMovieForm: Choice[] = [
+    { name: 'name', message: 'Name' },
+    { name: 'director', message: 'Director' },
+    { name: 'language', message: 'Language' },
+    { name: 'running_time', message: 'Running Time (min)' },
+    { name: 'year', message: 'Year' },
+    { name: 'explosions_count', message: 'Explosions Count' },
+  ];
+
+  horrorMovieForm: Choice[] = [
+    { name: 'name', message: 'Name' },
+    { name: 'director', message: 'Director' },
+    { name: 'language', message: 'Language' },
+    { name: 'running_time', message: 'Running Time (min)' },
+    { name: 'year', message: 'Year' },
+    { name: 'jump_scares_count', message: 'Jump Scares Count' },
+  ];
 
   async start() {
     /* YOUR CODE HERE */
